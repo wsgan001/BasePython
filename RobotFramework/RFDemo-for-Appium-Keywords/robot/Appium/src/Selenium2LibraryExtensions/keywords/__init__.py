00f6ac0d7ee0e6eaacb3a8de3cd3417692e7d488
Defias <Defias@GolukdeMac-mini.local>
2017-05-18 11:32:18 +0800

    update

--- RobotFramework/RFDemo-for-Appium-Keywords/robot/Appium/src/Selenium2LibraryExtensions/keywords/__init__.py
+++
@@ -1,9 +0,0 @@
-from _pagetests import _PageTestsKeywords
-from _draganddrop import _DragAndDropKeywords
-from _actionchains import _ActionChainsKeywords
-
-__all__ = [
-    "_PageTestsKeywords",
-    "_DragAndDropKeywords",
-    "_ActionChainsKeywords"
-]
--- RobotFramework/var.py
+++
@@ -1,8 +0,0 @@
-#coding=utf8
-import random
-__all__ = ['testurl', 'searchtext', 'LIST__vars', 'integer']
-
-testurl = 'http://www.baidu.com'
-searchtext = u'文件形式搜索'
-LIST__vars = ['hello', 'list', 'world']
-integer = random.randint(1, 10)
